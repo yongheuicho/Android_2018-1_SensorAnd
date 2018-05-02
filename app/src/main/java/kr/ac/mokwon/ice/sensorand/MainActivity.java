0f3bff11e00baa0f3cf8ca7a0b02820776265d63
yongheuicho <yongheuicho@gmail.com>
2018-05-02 09:52:05 +0900

    [2018-05-02] SensorAnd #1

--- app/src/main/java/kr/ac/mokwon/ice/sensorand/MainActivity.java
+++ app/src/main/java/kr/ac/mokwon/ice/sensorand/MainActivity.java
@@ -6,10 +6,21 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     protected BluetoothAdapter bthAdapter;
     protected BluetoothManager bthManager;
+    protected Button btFind, btConnect, btRead, btWrite;
+    protected EditText edWrite;
+    protected TextView txRead;
+
+    protected void showMsg(String str) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
@@ -18,10 +29,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bthManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
         if (bthManager == null) return;
+        showMsg("BluetoothManager is found.");
         bthAdapter = bthManager.getAdapter();
         if (bthAdapter == null) return;
+        showMsg("BluetoothAdapter is found.");
         if (!bthAdapter.isEnabled()) {
 
-        }
+            showMsg("Bluetooth is not enabled.");
+        } else showMsg("Bluetooth is enabled.");
+
+        btFind = (Button) findViewById(R.id.btFind);
+        btConnect = (Button) findViewById(R.id.btConnect);
+        btRead = (Button) findViewById(R.id.btRead);
+        btWrite = (Button) findViewById(R.id.btWrite);
+        edWrite = (EditText) findViewById(R.id.edWrite);
+        txRead = (TextView) findViewById(R.id.txRead);
     }
 }
