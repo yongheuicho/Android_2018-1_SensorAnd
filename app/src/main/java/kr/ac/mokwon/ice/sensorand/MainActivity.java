f14efb0772a5740f96877a02e629f8de9f99eff6
yongheuicho <yongheuicho@gmail.com>
2018-05-09 10:06:16 +0900

    [2018-05-09] SensorAnd #13

--- app/src/main/java/kr/ac/mokwon/ice/sensorand/MainActivity.java
+++ app/src/main/java/kr/ac/mokwon/ice/sensorand/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected Button btFind, btConnect, btRead, btWrite;
     protected EditText edWrite;
     protected TextView txRead;
+    protected StringTok stSensorInput = new StringTok("");
 
     protected void showMsg(String str) {
         Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
@@ -101,7 +102,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String str = bthService.getSerialInput();
-                txRead.setText(str);
+                stSensorInput.appendString(str);
+                txRead.setText(stSensorInput.toString());
             }
         });
 
--- myPush.bat
+++ myPush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-05-02] SensorAnd #12"
+git commit -m "[2018-05-09] SensorAnd #13"
 git push -u origin master
